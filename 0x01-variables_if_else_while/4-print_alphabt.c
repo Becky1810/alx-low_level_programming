54828a039eac5cee1a321655dcf529171f72d277
becky1810 <berikisuharuna456@gmail.com>
2022-03-13 04:51:58 -0700

    my first commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+
+
+/**
+
+ * main - Prints some letters of alphabet
+
+ *
+
+ * Return: Always (Success)
+
+ **/
+
+int main(void)
+
+{
+
+	char c;
+
+
+
+	for (c = 'a'; c <= 'z'; c++)
+
+	{
+
+		if (c != 'e' && c != 'q')
+
+		{
+
+			putchar(c);
+
+		}
+
+	}
+
+
+
+	putchar('\n');
+
+
+
+	return (0);
+
+}
+
+© 2022 GitHub, Inc.
+
+Terms
+
+Privacy
+
+Security
+
+Status
+
+
