39013bd508466ffc13d7646e47f1e6ad126dc516
Becky1810 <berikisuharuna456@gmail.com>
2022-03-13 14:38:02 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,14 +3,10 @@
 
 
 /**
-
-  * main - Prints 3 combination of numbers
-
-  *
-
-  * Return: Always (Success)
-
-  */
+ * main - Prints 3 combination of numbers
+ *
+ * Return: Always (Success)
+ */
 
 int main(void)
 
