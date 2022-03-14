95446d07bd5f5a0b51f0bd0946d2bcb5f49b3bc8
Becky1810 <berikisuharuna456@gmail.com>
2022-03-14 10:57:26 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Prints 3 combination of numbers
- *
- * Return: Always (Success)
- */
+  * main - Prints 3 combination of numbers
+  *
+  * Return: Always (Success)
+  */
 int main(void)
 {
 	int c, i, k, j;
-	
+
 	for (c = 48; c <= 57; c++)
-        {
-		for (i = 48; i <= 57; i++
+	{
+		for (i = 48; i <= 57; i++)
 		{
 			for (k = 48; k <= 57; k++)
 			{
@@ -24,7 +24,7 @@ int main(void)
 						putchar(' ');
 						putchar(k);
 						putchar(j);
-						
+
 					if (c + i + k + j == 227 && c == 57)
 					{
 					break;
