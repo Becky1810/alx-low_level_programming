7eb88707d00e40ef5203877897b03ddc39a356c3
Becky1810 <berikisuharuna456@gmail.com>
2022-03-16 14:05:06 +0100

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,16 +11,12 @@ int main(void)
 {
 	char c[] = "_putchar";
 	int i = 0;
-	
-while (c[i] != '\0')
+	while (c[i] != '\0')
 {
 	_putchar(c[i]);
 
 	i++;
-
 }
 	_putchar('\n');
-	
-return (0);
-
+	return (0);
 }	      
